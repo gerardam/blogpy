677f7dd6a6a305700297e5c3330ce22f1726bfb0
Gerardo Alvarez Mendoza <gerardam@outlook.com>
2019-07-11 11:42:31 -0500

    Edicion del archivo post html para mostrar la lista de post

--- aplicaciones/blog/urls.py
+++ aplicaciones/blog/urls.py
@@ -3,6 +3,7 @@ from .views import *
 
 urlpatterns = [
     path('', home, name = 'index'),
+    path('<slug:slug>/', detallePost, name = 'detalle_post'),
     path('programacion', programacion, name = 'programacion'),
     path('tecnologia', tecnologia, name = 'tecnologia'),
     path('alimentos', alimentos, name = 'alimentos'),
--- aplicaciones/blog/views.py
+++ aplicaciones/blog/views.py
@@ -1,13 +1,22 @@
 from django.shortcuts import render
+from .models import Post, Categoria
 
 def home(request):
-    return render(request, 'index.html')
+    posts = Post.objects.filter(estado = True)
+    return render(request, 'index.html', {'posts':posts})
+
+def detallePost(request, slug):
+    post = Post.objects.get(slug = slug)
+    return render(request, 'post.html', {'detalle_post':post})
 
 def programacion(request):
-    return render(request, 'programacion.html')
+    posts = Post.objects.filter(estado = True, categoria = Categoria.objects.get(nombre = 'Programacion'))
+    return render(request, 'programacion.html', {'posts': posts})
 
 def tecnologia(request):
-    return render(request, 'tecnologia.html')
+    posts = Post.objects.filter(estado = True, categoria = Categoria.objects.get(nombre = 'Tecnologia'))
+    return render(request, 'tecnologia.html', {'posts': posts})
 
 def alimentos(request):
-    return render(request, 'alimentos.html')
+    posts = Post.objects.filter(estado = True, categoria = Categoria.objects.get(nombre = 'Alimentos'))
+    return render(request, 'alimentos.html', {'posts': posts})
