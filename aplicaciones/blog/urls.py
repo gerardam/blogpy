16983d44233952dbfc09a56d0fe75e6d8ad81e39
Gerardo Alvarez Mendoza <gerardam@outlook.com>
2019-07-11 18:00:18 -0500

    Visualizado de contenidon los post, correccion de errores menores

--- aplicaciones/blog/urls.py
+++ aplicaciones/blog/urls.py
@@ -3,8 +3,8 @@ from .views import *
 
 urlpatterns = [
     path('', home, name = 'index'),
-    path('<slug:slug>/', detallePost, name = 'detalle_post'),
     path('programacion', programacion, name = 'programacion'),
     path('tecnologia', tecnologia, name = 'tecnologia'),
     path('alimentos', alimentos, name = 'alimentos'),
+    path('<slug:slug>/', detallePost, name = 'detalle_post'),
 ]
--- aplicaciones/blog/views.py
+++ aplicaciones/blog/views.py
@@ -1,22 +1,24 @@
 from django.shortcuts import render
 from .models import Post, Categoria
+from django.shortcuts import get_object_or_404
 
 def home(request):
     posts = Post.objects.filter(estado = True)
     return render(request, 'index.html', {'posts':posts})
 
 def detallePost(request, slug):
-    post = Post.objects.get(slug = slug)
+    #post = Post.objects.get(slug = slug)
+    post = get_object_or_404(Post, slug = slug)
     return render(request, 'post.html', {'detalle_post':post})
 
 def programacion(request):
-    posts = Post.objects.filter(estado = True, categoria = Categoria.objects.get(nombre = 'Programacion'))
+    posts = Post.objects.filter(estado = True, categoria = Categoria.objects.get(nombre__iexact = 'Programacion'))
     return render(request, 'programacion.html', {'posts': posts})
 
 def tecnologia(request):
-    posts = Post.objects.filter(estado = True, categoria = Categoria.objects.get(nombre = 'Tecnologia'))
+    posts = Post.objects.filter(estado = True, categoria = Categoria.objects.get(nombre__iexact = 'Tecnologia'))
     return render(request, 'tecnologia.html', {'posts': posts})
 
 def alimentos(request):
-    posts = Post.objects.filter(estado = True, categoria = Categoria.objects.get(nombre = 'Alimentos'))
+    posts = Post.objects.filter(estado = True, categoria = Categoria.objects.get(nombre__iexact = 'Alimentos'))
     return render(request, 'alimentos.html', {'posts': posts})
